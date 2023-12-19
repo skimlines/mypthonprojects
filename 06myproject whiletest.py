6cddf9b05d226201e040c1d782a82bbc7517c426
skimlines <117303424+skimlines@users.noreply.github.com>
2023-12-19 10:56:28 -0500

    Add files via upload

--- 06myproject whiletest.py
+++ 06myproject whiletest.py
@@ -1,14 +1,29 @@
 # This program takes a string of letters from the user and sorts them alphabetically. It does this by taking the string, converting it to a list, then sorting it alphabeitcally, and then it converts the list back into a string.
+# This program checks for duplicate letters!!!
 
 graveyard = ''
 answer = ''
 
+# while loop to keep the programming running until 'quit'
 while answer != "quit":
+
     answer = input("What to add? ")
     print ("Type quit to quit")
 
     graveyard += answer
+
+    # Checks for duplicates and only adds unique letters
+    new_list = []
+    for each_letter in graveyard:
+        if each_letter not in new_list:
+            new_list.append(each_letter)
+#    print(new_list)
+    graveyard = new_list[:]
+    del new_list
+
     graveyard = sorted(graveyard)
+
+    # list -> string
     graveyard = ''.join(graveyard)
 
     print("The graveyard contains: ", graveyard)
